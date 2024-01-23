726c293626684bcaace6ef415a2422661fea2572
Juan Jimenez <sr.j_599@hotmail.com>
2024-01-23 18:30:55 -0500

    tarea7

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,8 +2,18 @@
 <h1 v-if="!pokemonGanador">Espere por favor mientras carga...</h1>
   <div  v-else >
     <h1>Adivina el Pokemon</h1>
-    <PokemonImagen :pokemonId="pokemonGanador.id" :showPokemon="mostrarPokemon" />
-    <PokemonOpciones @selecciono="revisarClick($event)" :pokemons="pokemonsArr" />
+    <PokemonImagen  :pokemonId="pokemonGanador.id" :showPokemon="mostrarPokemon" />
+    <PokemonOpciones v-if="pantalla" @selecciono="revisarClick($event)" :pokemons="pokemonsArr" />
+  </div >
+  <div v-if="pantallaGanador" class="ganador">
+
+    <h2>Felicidades, has ganado el juego!!</h2>
+    <button @click="reiniciar()">Reiniciar juego</button>
+  </div>
+  <div v-if="pantallaPerdedor" class="perdedor">
+
+    <h2>Has fallado, intentalo de nuevo</h2>
+    <button @click="reiniciar()">Reiniciar juego</button>
   </div>
 </template>
 
@@ -62,7 +72,29 @@ export default {
     revisarClick(datoRecibido){
       console.log('dio click y reporto desde el padre')
       console.log(datoRecibido.hobby)
-      this.mostrarPokemon=true
+      
+
+      if (this.pokemonGanador.id=== datoRecibido.id) {
+        this.mostrarPokemon=true
+        this.pantalla=false
+        this.pantallaGanador=true
+        
+      }else{
+        this.pantalla=false
+        this.pantallaPerdedor=true
+      }
+
+    },
+    reiniciar(){
+     
+      this.pokemonsArr = [];
+    this.pokemonGanador = null;
+    this.mostrarPokemon = false;
+    this.pantallaGanador = false;
+    this.pantallaPerdedor = false;
+    this.pantalla = true;
+    this.cargaInicial();
+        
     }
   
   },
@@ -70,11 +102,35 @@ export default {
       return {
         pokemonsArr: [],
         pokemonGanador:null,
-        mostrarPokemon:false
+        mostrarPokemon:false,
+        pantallaGanador:false,
+        pantallaPerdedor:false,
+        pantalla:true
       };
     },
 };
 </script>
 
 <style>
+
+h1{
+  font-size: 40px;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  color: rgb(42, 42, 100);
+}
+.perdedor{
+  color: brown;
+}
+button{
+
+    background:rgb(250, 255, 255);
+    border-radius: 5px;
+    border: 3px solid rgba(51, 13, 13, 0.2);
+    width: 150px;
+    padding: 5px;
+    cursor: pointer;
+}
+button:hover{
+    background: rgba(0, 0, 0, 0.1);
+}
 </style>
